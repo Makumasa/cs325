9468c3a15b59bc7640be66222bde956e743bd137
wilkinsw <wilkinsw@cobaltcreek.com>
2017-01-31 12:50:14 -0800

    initial naive function

--- a1/a1.py
+++ a1/a1.py
@@ -1,5 +1,5 @@
 from math import hypot
-import time
+import timeit
 
 def read_input(filepath="example.input"):
     data_list = []
@@ -14,30 +14,59 @@ def read_input(filepath="example.input"):
 
     return data_list
 
-def n_squared(data):
+def brute_force(data):
     smallest_distance = None
     points = []
     for x1, y1 in data:
         for x2, y2 in data:
             result = hypot(x2-x1, y2-y1)
             if result != 0:
-                if smallest_distance is None:
+                if result < smallest_distance or smallest_distance is None:
                     smallest_distance = result
                     points = [((x1, y1), (x2, y2))]
-                else:
-                    if result < smallest_distance:
+                elif result == smallest_distance:
+                    if ((x2, y2), (x1, y1)) not in points:
+                        points.append(((x1, y1), (x2, y2)))
+
+    return smallest_distance, points
+
+def naive(data):
+    n = len(data)
+    smallest_distance = None
+    points = []
+    if n <= 3:
+        for x1, y1 in data:
+            for x2, y2 in data:
+                result = hypot(x2 - x1, y2 - y1)
+                if result != 0:
+                    if result < smallest_distance or smallest_distance is None:
                         smallest_distance = result
                         points = [((x1, y1), (x2, y2))]
                     elif result == smallest_distance:
                         if ((x2, y2), (x1, y1)) not in points:
                             points.append(((x1, y1), (x2, y2)))
+    else:
+        m = n/2
+        data1 = data[:m]
+        data2 = data[m:]
+        distance1, points1 = naive(data1)
+        distance2, points2 = naive(data2)
+        if distance1 == distance2:
+            smallest_distance = distance1
+            points = points1 + points2
+        elif distance1 > distance2:
+            smallest_distance = distance2
+            points = points2
+        elif distance1 < distance2:
+            smallest_distance = distance1
+            points = points1
 
     return smallest_distance, points
 
 if __name__ == "__main__":
     data = read_input()
     start = time.time()
-    output = n_squared(data)
+    output = naive(data)
     stop = time.time()
     print "Completed in:", stop-start
 
